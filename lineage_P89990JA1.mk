f94108534074c473956a3530dc3465a6c9cfe8b2
Yuyuko1024 <dingwenxuan4@gmail.com>
2022-09-28 21:21:22 +0800

    P89990JA1: Use tablet WiFI only config

--- lineage_P89990JA1.mk
+++ lineage_P89990JA1.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 # Inherit from P89990JA1 device
 $(call inherit-product, device/lenovo/P89990JA1/device.mk)
