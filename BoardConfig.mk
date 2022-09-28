14a1da537f82897f34679d9427e68852b88b2416
kubersharma001 <kubersharma001@gmail.com>
2022-09-28 16:55:03 +0800

    P89990JA1: Initial props import

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,6 +110,9 @@ BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := bengal
 
 # Properties
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/properties/product.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/properties/system_ext.prop
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 
 # Releasetools
