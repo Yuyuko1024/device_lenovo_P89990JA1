735e06ab0142940819019e2d68c9d85ab8aae5f4
daniml3 <danimoral1001@gmail.com>
2022-10-20 12:44:30 +0800

    P89990JA1: lights: turn into an AIDL service * Based on AOSP's default lights AIDL implementation * Remove unused functions * Remove unused lights * Refactor specific lights handling * Stop overriding the stock lights HAL vintf entry
    Signed-off-by: daniml3 <danimoral1001@gmail.com>

--- device.mk
+++ device.mk
@@ -87,7 +87,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.lenovo
+    android.hardware.lights-service.lenovo
 
 # Overlays
 PRODUCT_PACKAGES += \
